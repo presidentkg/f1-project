72700045102796028006b55b28d4696257848611
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-07 16:48:23 +0200

    Adjusted positioning and made the logo into a link

--- components/nav-bar/nav-bar.tsx
+++ components/nav-bar/nav-bar.tsx
@@ -5,8 +5,10 @@ import Link from "next/link";
 export default function NavBar() {
     return (
         <nav className="w-full h-16 bg-gray-800 text-white flex items-center px-4">
-            <Image src="/F1Logo.svg" alt="F1 Logo" width={100} height={40} className="mr-4" />
-            <ul className="flex space-x-8 ml-auto">
+            <Link href="/">
+                <Image src="/F1Logo.svg" alt="F1 Logo" width={100} height={40} className="mr-4" />
+            </Link>
+            <ul className="flex space-x-20 ml-auto mr-auto">
                 <li>
                     <Link href="/schedule" className="text-xl">Schedule</Link>
                     <span className="ml-2 mr-2">▽</span>
