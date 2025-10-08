fc2bc009d1d1448a05827c8710e2d476b4746725
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-08 16:00:53 +0200

    Created new interfaces for fetching standings

--- lib/types/interfaces.ts
+++ lib/types/interfaces.ts
@@ -13,14 +13,15 @@ export interface CurrentDriver extends Driver {
     teamId: string;
 }
 
-export interface DriversF1apiResponse {
+export interface CurrentDriversF1apiResponse {
   api: string;
   url: string;
   limit: number;
+  offset: number;
   total: number;
   season: number;
   championshipId: string;
-  drivers: Driver[];
+  drivers: CurrentDriver[];
 }
 
 export interface Team {
@@ -32,6 +33,39 @@ export interface Team {
     driverChampionships: number;
     url: string;
 }
+
+export interface DriverChampionshipApiResponse {
+    api: string;
+    url: string;
+    limit: number;
+    offset: number;
+    total: number;
+    season: number;
+    championshipId: string;
+    drivers_championship: DriverChampionshipEntry[];
+}
+
+export interface DriverChampionshipEntry {
+    classificationId: number;
+    driverId: string;
+    teamId: string;
+    points: number;
+    position: number;
+    wins: number;
+    driver: Omit<Driver, 'driverId'>;
+    team: DriverChampionshipTeamResponse;
+}
+
+export interface DriverChampionshipTeamResponse{
+    teamId: string;
+    teamName: string;
+    country: string;
+    firstAppareance: number;
+    constructorsChampionships: number;
+    driverChampionships: number;
+    url: string;    
+}
+
 
 export interface Circuit {
     circuitId: string;
