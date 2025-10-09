425df7913c28b059af0bda730f74bd350e0f0c29
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-09 15:35:54 +0200

    Corrected the table header

--- components/tables/team-standings-table.tsx
+++ components/tables/team-standings-table.tsx
@@ -9,7 +9,7 @@ export default async function TeamStandingsTable() {
     const standings = TeamStandingsDataToTableData(standingsData);
     return (
         <section className="bg-rose-50 p-6 lg:max-w-3/4 lg:mx-auto">
-            <h1 className="text-3xl font-extrabold mb-2">{season} DRIVERS' STANDINGS</h1>
+            <h1 className="text-3xl font-extrabold mb-2">{season} TEAMS' STANDINGS</h1>
             <div className="overflow-x-auto">
                 <table className="bg-slate-100 min-w-full">
                     <thead className="bg-slate-200">
