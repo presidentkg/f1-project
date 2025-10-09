9800b949d597e30d590896000ab74a47ff8072a1
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-09 15:32:46 +0200

    Created team standings table component

---
+++ components/tables/team-standings-table.tsx
@@ -0,0 +1,34 @@
+import { FetchStandingsConstructorsChampionship } from "@/lib/data/standings";
+import { TeamStandingsDataToTableData } from "@/lib/utils/transform-data";
+
+export default async function TeamStandingsTable() {
+    const standingsData = await FetchStandingsConstructorsChampionship();
+    if (!standingsData)
+        return <p>Failed to load the Constructors Championship standings</p>
+    const season = standingsData.season;
+    const standings = TeamStandingsDataToTableData(standingsData);
+    return (
+        <section className="bg-rose-50 p-6 lg:max-w-3/4 lg:mx-auto">
+            <h1 className="text-3xl font-extrabold mb-2">{season} DRIVERS' STANDINGS</h1>
+            <div className="overflow-x-auto">
+                <table className="bg-slate-100 min-w-full">
+                    <thead className="bg-slate-200">
+                        <tr className="text-left">
+                            <th className="px-2 sm:px-4 py-2 sm:tracking-widest">POS.</th>
+                            <th className="px-2 sm:px-4 py-2 sm:tracking-widest">TEAM</th>
+                            <th className="px-2 sm:px-4 py-2 sm:tracking-widest">PTS.</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {standings.map((team) => (
+                            <tr key={team.team} className="hover:bg-slate-50">
+                                <td className="px-2 sm:px-4 py-2 font-bold">{team.position}</td>
+                                <td className="px-2 sm:px-4 py-2">{team.team}</td>
+                                <td className="px-2 sm:px-4 py-2 w-10">{team.points}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    )}
