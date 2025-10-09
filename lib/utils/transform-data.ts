a5b60b30a86c2d8be28d8741090583948899528a
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-09 15:30:30 +0200

    Added functions to transform api data to table data

--- lib/utils/transform-data.ts
+++ lib/utils/transform-data.ts
@@ -1,12 +1,12 @@
 import { Race, Result, RaceResults } from '../types/race';
+import { DriverChampionshipApiResponse, DriverChampionshipEntry, DriversStandings, ConstructorsChampionshipApiResponse, TeamStandings, ConstructorsChampionshipEntry } from '../types/standings';
 
 export function RaceDataToTableData(apiRaceData: Race): RaceResults[] {
 
     const results: Result[] = apiRaceData.results;
 
-    if (!results || results.length === 0) {
+    if (!results || results.length === 0)
         return [];
-    }
 
     return results.map(result => ({
         position: result.position,
@@ -15,6 +15,30 @@ export function RaceDataToTableData(apiRaceData: Race): RaceResults[] {
         team: transformTeamName(result.team.teamName),
         time: result.time,
         points: result.points, 
+    }));
+}
+
+export function DriversStandingsDataToTableData(apiStandingsData: DriverChampionshipApiResponse): DriversStandings[] {
+    const driversStandings: DriverChampionshipEntry[] = apiStandingsData.drivers_championship
+    if (!driversStandings || driversStandings.length === 0)
+        return [];
+    return driversStandings.map(driver => ({
+        position: driver.position,
+        driver: transformDriverName(`${driver.driver.name} ${driver.driver.surname}`),
+        nationality: driver.driver.nationality,
+        team: transformTeamName(driver.team.teamName),
+        points: driver.points,
+    }));
+}
+
+export function TeamStandingsDataToTableData(apiStandingsData: ConstructorsChampionshipApiResponse): TeamStandings[] {
+    const teamStandings: ConstructorsChampionshipEntry[] = apiStandingsData.constructors_championship;
+    if (!teamStandings || teamStandings.length === 0)
+        return [];
+    return teamStandings.map(team => ({
+        position: team.position,
+        team: transformTeamName(team.team.teamName),
+        points: team.points,
     }));
 }
 
