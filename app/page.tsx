b3019144bb4802cd9c0b9ee935defbd1bd31efa4
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-09 14:56:47 +0200

    Added styling to table

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import LastRaceTable from "@/components/home-page-tables/last-race-table";
+import LastRaceTable from "@/components/tables/last-race-table";
 
 
 export default function Home() {
   return (
     <section>
-      <h1>Latest Race</h1>
       <LastRaceTable />
     </section>
   );
