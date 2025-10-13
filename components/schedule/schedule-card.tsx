2c32c98c077cdf09aa2aa8eed9d0f6cb5e674373
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-13 16:42:46 +0200

    Created schedule card component

---
+++ components/schedule/schedule-card.tsx
@@ -0,0 +1,29 @@
+import { RaceItem } from "@/lib/types/race";
+import { raceIdToTitle } from "@/lib/utils/transform-data";
+
+
+export default async function ScheduleCard( { race, season } : {race : RaceItem, season: number}) {
+    return (
+        <div className="flex justify-between items-start p-4 rounded-xl shadow-md bg-rose-50">
+            <div className="flex flex-col justify-between h-full gap-4">
+                <h2 className="text-lg font-semibold tracking-wide">Round {race.round}</h2>
+                <h1 className="text-2xl font-extrabold tracking-wide">{raceIdToTitle(race, season)}</h1>
+                
+                {race.winner ? (
+                    <div>
+                        <p> Winner: {race.winner.name} {race.winner.surname}</p>
+                    </div>
+                )
+                : (
+                    <div>
+                        <p>{race.schedule.fp1.date} - {race.schedule.race.date} </p>
+                    </div>
+                )}
+            </div>
+            <div>
+                <p>{race.schedule.fp1.date} <br /> {race.schedule.race.date} </p>
+            </div>
+        </div>
+    )
+
+}
