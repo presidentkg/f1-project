b4cf9031b7c4ed6b16b7b2ebc18e43693b4c2f8a
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-14 11:13:53 +0200

    Updated function names

--- components/schedule/schedule-card.tsx
+++ components/schedule/schedule-card.tsx
@@ -4,7 +4,7 @@ import { raceIdToTitle } from "@/lib/utils/transform-data";
 
 export default async function ScheduleCard( { race, season } : {race : RaceItem, season: number}) {
     return (
-        <div className="flex justify-between items-start p-4 rounded-xl shadow-md bg-rose-50">
+        <section className="flex justify-between items-start p-4 rounded-xl shadow-md bg-rose-50">
             <div className="flex flex-col justify-between h-full gap-4">
                 <h2 className="text-lg font-semibold tracking-wide">Round {race.round}</h2>
                 <h1 className="text-2xl font-extrabold tracking-wide">{raceIdToTitle(race, season)}</h1>
@@ -23,7 +23,7 @@ export default async function ScheduleCard( { race, season } : {race : RaceItem,
             <div>
                 <p>{race.schedule.fp1.date} <br /> {race.schedule.race.date} </p>
             </div>
-        </div>
+        </section>
     )
 
 }
--- lib/utils/get-team-name-by-id.ts
+++ lib/utils/get-team-name-by-id.ts
@@ -1,9 +1,9 @@
 import { FetchCurrentTeamById } from "@/lib/data/teams";
-import { TransformTeamApiResponseToTeam, TransformTeamName } from "@/lib/utils/transform-data";
+import { transformTeamApiResponseToTeam, transformTeamName } from "@/lib/utils/transform-data";
 import { Team, TeamApiResponse } from "@/lib/types/team";
 
 export async function getTeamNameById(teamId: string): Promise<string | null> {
     const teamApi: TeamApiResponse | null = await FetchCurrentTeamById(teamId);
-    const team: Team | null = teamApi ? TransformTeamApiResponseToTeam(teamApi) : null;
-    return team ? TransformTeamName(team.teamName) : null;
+    const team: Team | null = teamApi ? transformTeamApiResponseToTeam(teamApi) : null;
+    return team ? transformTeamName(team.teamName) : null;
 }
