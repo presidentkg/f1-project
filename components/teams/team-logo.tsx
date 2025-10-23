86d513e0c840cd7446f15decc1da59b9d44a5678
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-23 14:42:10 +0200

    Changed img to Image

--- components/teams/team-logo.tsx
+++ components/teams/team-logo.tsx
@@ -1,5 +1,6 @@
 import { TeamApiResponseTeam } from '@/lib/types/team';
 import { getTeamColor } from "@/lib/utils/colors";
+import Image from 'next/image';
 
 export default function TeamLogo({ team }: { team: TeamApiResponseTeam }){
     const teamId = team.teamId;
@@ -10,7 +11,7 @@ export default function TeamLogo({ team }: { team: TeamApiResponseTeam }){
             className="flex items-center justify-center p-4"
             style={{ backgroundColor: teamColor }}
         >
-            <img 
+            <Image 
                 src={`/team-logo/${teamId}.svg`} 
                 alt={`${team.teamName} logo`} 
                 className="w-40 h-40 object-contain"
