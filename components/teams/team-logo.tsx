9c2c82d4fb748d12d67b791bc55ced98f65d9492
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-27 14:45:46 +0100

    Moved team bg color from team logo to Team page

--- components/teams/team-logo.tsx
+++ components/teams/team-logo.tsx
@@ -1,15 +1,12 @@
 import { TeamApiResponseTeam } from '@/lib/types/team';
-import { getTeamColor } from "@/lib/utils/colors";
 import Image from 'next/image';
 
 export default function TeamLogo({ team }: { team: TeamApiResponseTeam }){
     const teamId = team.teamId;
-    const teamColor = getTeamColor(team.teamId);
 
     return(
         <div 
             className="flex items-center justify-center p-4"
-            style={{ backgroundColor: teamColor }}
         >
             <Image 
                 src={`/team-logo/${teamId}.svg`} 
