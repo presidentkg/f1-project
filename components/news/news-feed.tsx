c613d085ef68bcc1ab5c110a0c885dd58c7e8431
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-27 23:01:27 +0100

    Changed so news feed recives newsLimit and changed styling

--- components/news/news-feed.tsx
+++ components/news/news-feed.tsx
@@ -1,11 +1,11 @@
 import fetchLatestF1News from "../../lib/data/news";
 import NewsCard from "./news-card";
 
-export default async function NewsFeed() {
-    const articles = await fetchLatestF1News();
+export default async function NewsFeed({newsLimit} : {newsLimit: number}) {
+    const articles = await fetchLatestF1News({ newsLimit });
     return (
         <section className="flex flex-col gap-6 mb-12 md:max-w-1/2 lg:max-w-1/4 mx-auto items-center">
-            <h1 className="text-2xl lg:text-4xl font-extrabold text-slate-900 underline tracking-wide"> Latest F1 News</h1>
+            <h1 className="text-2xl lg:text-3xl font-extrabold text-slate-900 tracking-wide"> Latest F1 news from f1.com</h1>
             {articles.map((article) => (
                 <div className="w-full" key={article.guid ?? article.link}>
                     <NewsCard article={article} />
