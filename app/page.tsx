d7ff71e9441e5b3512091ade4b1cada82faf75b1
Karl Ragnar <karlragnar.kling@gmail.com>
2025-10-27 23:02:50 +0100

    Improved styling and added a link to news page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import NewsFeed from "@/components/news/news-feed";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,7 +9,14 @@ export default function Home() {
         <p className="mt-8 text-lg text-slate-700">Welcome to the F1 Info Hub, your go-to source for all things Formula 1.</p>
         <p className="mt-4 mb-8 text-base text-slate-600">Here you can find race schedules and results, read driver and team profiles, and view up-to-date driver and constructor standings.</p>
       </section>
-      <NewsFeed />
+      <hr className="my-8 border-slate-200" />
+      <NewsFeed newsLimit={3}/>
+      <Link
+        href="/news"
+        className={"px-4 py-2 rounded bg-[#EE0000] text-white hover:bg-[#d10202] block w-max mx-auto"}
+      >
+        See more news
+      </Link>
     </main>
   );
 }
